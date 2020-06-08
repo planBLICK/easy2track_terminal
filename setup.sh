ec01a6ac5eda645bee927490522c203377d55183
Florian Kroeber <florian.kroeber@website-doktor.com>
2020-06-08 14:17:51 +0200

    make sure we can checkout a new version on update

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /usr/bin/
-./git clone "https://github.com/planBLICK/easy2track_terminal.git" "/home/pi/easy2track/" 2> /dev/null || (cd "/home/pi/easy2track/" ; git pull)
+./git clone "https://github.com/planBLICK/easy2track_terminal.git" "/home/pi/easy2track/" 2> /dev/null || (cd "/home/pi/easy2track/"; git fetch --all; git reset --hard origin/live;)
 chmod +x /home/pi/easy2track/setup.sh
 chmod +x /home/pi/easy2track/app/run.sh
 /home/pi/easy2track/setup.sh
