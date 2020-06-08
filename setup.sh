972f5b0b9150a268f04483fcf4ccdc9b4f614b62
Florian Kroeber <florian.kroeber@website-doktor.com>
2020-06-08 14:15:31 +0200

    try to run everything automatically in a smart way

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@
 
 cd /usr/bin/
 ./git clone "https://github.com/planBLICK/easy2track_terminal.git" "/home/pi/easy2track/" 2> /dev/null || (cd "/home/pi/easy2track/" ; git pull)
+chmod +x /home/pi/easy2track/setup.sh
+chmod +x /home/pi/easy2track/app/run.sh
+/home/pi/easy2track/setup.sh
+
+sudo systemctl edit --force --full easy2track_terminal.service
+systemctl status easy2track_terminal.service
+sudo systemctl enable easy2track_terminal.service
+sudo systemctl start easy2track_terminal.service
