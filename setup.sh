6dc5216916b04004abf026e5532e783ae867b7ec
Florian Kroeber <florian.kroeber@website-doktor.com>
2020-06-08 14:23:32 +0200

    reset checked out version before pulling latest; copy service file instead opf editing in place

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 cd /usr/bin/
-./git clone "https://github.com/planBLICK/easy2track_terminal.git" "/home/pi/easy2track/" 2> /dev/null || (cd "/home/pi/easy2track/"; git fetch --all; git reset --hard origin/live;)
+./git clone "https://github.com/planBLICK/easy2track_terminal.git" "/home/pi/easy2track/" 2> /dev/null || (cd "/home/pi/easy2track/"; git reset --hard origin/live; git pull; )
 chmod +x /home/pi/easy2track/setup.sh
 chmod +x /home/pi/easy2track/app/run.sh
 /home/pi/easy2track/setup.sh
 
-sudo systemctl edit --force --full easy2track_terminal.service
+sudo cp /home/pi/easy2track/easy2track_terminal.service /etc/systemd/system
 systemctl status easy2track_terminal.service
 sudo systemctl enable easy2track_terminal.service
 sudo systemctl start easy2track_terminal.service
