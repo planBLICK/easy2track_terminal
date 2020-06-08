4cf72122a686a8125f26a800bd52fadf9ef94dc3
Florian Kroeber <florian.kroeber@website-doktor.com>
2020-06-08 14:26:06 +0200

    remove self calling setup

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ cd /usr/bin/
 ./git clone "https://github.com/planBLICK/easy2track_terminal.git" "/home/pi/easy2track/" 2> /dev/null || (cd "/home/pi/easy2track/"; git reset --hard origin/live; git pull; )
 chmod +x /home/pi/easy2track/setup.sh
 chmod +x /home/pi/easy2track/app/run.sh
-/home/pi/easy2track/setup.sh
 
 sudo cp /home/pi/easy2track/easy2track_terminal.service /etc/systemd/system
 systemctl status easy2track_terminal.service
