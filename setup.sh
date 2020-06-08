e67c9a70d2c92aba48b1c8b6a2260f649a437aa2
Florian Kroeber <florian.kroeber@website-doktor.com>
2020-06-08 15:29:02 +0200

    make git fetch

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /usr/bin/
-./git clone "https://github.com/planBLICK/easy2track_terminal.git" "/home/pi/easy2track/" 2> /dev/null || (cd "/home/pi/easy2track/"; git reset --hard origin/live; git pull; )
+./git clone "https://github.com/planBLICK/easy2track_terminal.git" "/home/pi/easy2track/" 2> /dev/null || (cd "/home/pi/easy2track/"; git fetch; git reset --hard origin/live;  git pull; )
 chmod +x /home/pi/easy2track/setup.sh
 chmod +x /home/pi/easy2track/app/run.sh
 
