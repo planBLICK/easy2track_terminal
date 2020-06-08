55399a32b004f98d6356399f65fd1c3fe765aed0
Florian Kroeber <florian.kroeber@website-doktor.com>
2020-06-08 18:29:41 +0200

    use greyscale instead of black&white

--- app/main.py
+++ app/main.py
@@ -178,7 +178,7 @@ class Easy2Track:
 
                 #image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
                 image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-                (thresh, image) = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+                #(thresh, image) = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
                 image = ImageOps.mirror(Image.fromarray(image))
 
                 codes = decode(image)
